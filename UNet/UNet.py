98f6d8b593af533868864cf661980b82665d2fcf
Duncan Boyd <duncan.boyd@sfmrc175.ahs.ucalgary.ca>
2022-06-14 12:14:20 -0600

    Updated real Unet to match complex

--- UNet/UNet.py
+++ UNet/UNet.py
@@ -1,30 +1,146 @@
 
-# Duncan Boyd, 11\05\2022
-# Uses a U-Net to reconstruct undersampled (kind of) photographs taken from a large dataset.
+# Basic Unet model that mirrors the complex Unet, but does not use the CompConv2D layer.
+# June 14, 2022
 
-# Libraries
+# Imports
+import os
+import time
+from re import I
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+import tensorflow as tf
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
-import keras as ks
-from keras.models import Model
-from keras import backend as K
+import keras
+from keras import layers
 from keras.layers import Input, Lambda, Conv2D, MaxPooling2D, concatenate, UpSampling2D
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.image as iio
 import glob
-import cv2
+from keras import backend as K
+from keras.models import Model
+import random
 
-# Constants
-BRAINS = 1
-PICS = 170
-SAMP_RATE = 5
-EPOCHS = 5
-TOTAL_PICS = 50
-TEST_PICS = 10
+# Variables
+EPOCHS = 1
+MOD = 1
+BATCH_SIZE = 5
+num_train = 20 # max 4254
+num_val = 5 # max 1700
+num_test = 5 # max 1700
+addr = '/Users/duncan.boyd/Documents/WorkCode/workvenv/MRIPractice/'
+
+init_time = time.time()
+print("\nInitialized\n")
+
+# Loss function
+def nrmse(y_true, y_pred):
+    denom = K.sqrt(K.mean(K.square(y_true), axis=(1,2,3)))
+    return K.sqrt(K.mean(K.square(y_pred - y_true), axis=(1,2,3)))\
+    /denom
+
+# IFFT layer, used in u_net.
+def ifft_layer(kspace):
+    real = layers.Lambda(lambda kspace : kspace[:,:,:,0])(kspace)
+    imag = layers.Lambda(lambda kspace : kspace[:,:,:,1])(kspace)
+    kspace_complex = tf.complex(real,imag)
+    rec1 = tf.abs(tf.ifft2d(kspace_complex))
+    rec1 = tf.expand_dims(rec1, -1)
+    return rec1
+
+# Upgraded version, returns fewer arrays but with a faster and more efficient method.
+# Still need to figure out the imaginary image domain stuff. 
+def get_brains(num_train, num_val, num_test, addr):
+
+    # Note: In train, one file is (174, 256, 256).
+    kspace_files_train = np.asarray(glob.glob(addr+"Train/*.npy")) 
+    kspace_files_val = np.asarray(glob.glob(addr+"Val/*.npy"))
+    kspace_files_test = np.asarray(glob.glob(addr+"Test/*.npy"))
+
+    print("train scans:", len(kspace_files_train))
+    print("val scans:", len(kspace_files_val))
+    print("test scans:", len(kspace_files_test))
+
+    samp_mask = np.load(addr+"mask.npy")
+    shape = (256, 256)
+    norm = np.sqrt(shape[0]*shape[1])
+
+    image_train = []
+    kspace_train = []
+    count = 0
+    for i in random.sample(range(0, len(kspace_files_train)), len(kspace_files_train)):
+        brain = np.load(kspace_files_train[i])/norm
+        if count >= num_train:
+                break
+        for i in random.sample(range(0, brain.shape[0]), brain.shape[0]):
+            image_train.append(np.abs(np.fft.ifft2(brain[i,:,:,0]+1j*brain[i,:,:,1])).astype(np.float64))
+            brain[i, samp_mask, : ] = 0
+            kspace_train.append(brain[i].astype(np.float64))
+            count += 1
+            if count >= num_train:
+                break
+    kspace_train = np.asarray(kspace_train)
+    print("kspace train:", kspace_train.shape)
+    image_train = np.asarray(image_train)
+    image_train = np.expand_dims(image_train, axis=3)
+    print("image train:", image_train.shape)
+
+    image_val = []
+    kspace_val = []
+    count = 0
+    for i in random.sample(range(0, len(kspace_files_val)), len(kspace_files_val)):
+        brain = np.load(kspace_files_val[i])/norm
+        if count >= num_val:
+                break
+        for i in random.sample(range(0, brain.shape[0]), brain.shape[0]):
+            image_val.append(np.abs(np.fft.ifft2(brain[i,:,:,0]+1j*brain[i,:,:,1])).astype(np.float64))
+            brain[i, samp_mask, : ] = 0
+            kspace_val.append(brain[i].astype(np.float64))
+            count += 1
+            if count >= num_val:
+                break
+    kspace_val = np.asarray(kspace_val)
+    print("kspace val:", kspace_val.shape)
+    image_val = np.asarray(image_val)
+    image_val = np.expand_dims(image_val, axis=3)
+    print("image val:", image_val.shape)
+
+    image_test = []
+    kspace_test = []
+    count = 0
+    for i in random.sample(range(0, len(kspace_files_test)), len(kspace_files_test)):
+        brain = np.load(kspace_files_test[i])/norm
+        if count >= num_test:
+                break
+        for i in random.sample(range(0, brain.shape[0]), brain.shape[0]):
+            image_test.append(np.abs(np.fft.ifft2(brain[i,:,:,0]+1j*brain[i,:,:,1])).astype(np.float64))
+            brain[i, samp_mask, : ] = 0
+            kspace_test.append(brain[i].astype(np.float64))
+            count += 1
+            if count >= num_test:
+                break
+    kspace_test = np.asarray(kspace_test)
+    print("kspace test:", kspace_test.shape)
+    image_test = np.asarray(image_test)
+    image_test = np.expand_dims(image_test, axis=3)
+    print("image test:", image_test.shape)
+
+    # Question: image_train seems to be the reconstructed images here, but it's imaginary. Why is image domain imaginary?
+    # Especially since the WNet returns a real image. Does the imaginary part in image domain yield any valuable information?
+    # For the time being, I'm just going to load the stats manually.
+    stats = np.load('/Users/duncan.boyd/Documents/WorkCode/workvenv/WNetPractice/stats.npy')
+    '''# save k-space and image domain stats
+    stats = np.zeros(4)
+    stats[0] = kspace_train.mean()
+    stats[1] = kspace_train.std()
+    aux = np.abs(image_train[:,:,:,0] +1j*image_train[:,:,:,1])
+    stats[2] = aux.mean()
+    stats[3] = aux.std()
+    np.save(STATS_ADDR, stats)'''
+
+    return stats, kspace_train, image_train, kspace_val, image_val, kspace_test, image_test
 
 # U-Net model.
-def u_net(H=256,W=256,channels = 1,kshape = (3,3)):
+def u_net(mu1,sigma1,mu2,sigma2, H=256,W=256,channels = 2,kshape = (3,3)):
     inputs = Input(shape=(H,W,channels))
 
     conv1 = Conv2D(48, kshape, activation='relu', padding='same')(inputs)
@@ -60,78 +176,48 @@ def u_net(H=256,W=256,channels = 1,kshape = (3,3)):
     conv7 = Conv2D(48, kshape, activation='relu', padding='same')(up3)
     conv7 = Conv2D(48, kshape, activation='relu', padding='same')(conv7)
     conv7 = Conv2D(48, kshape, activation='relu', padding='same')(conv7)
+
+    conv8 = layers.Conv2D(2, (1, 1), activation='linear')(conv7)
+    res1 = layers.Add()([conv8,inputs])
+    res1_scaled = layers.Lambda(lambda res1 : (res1*sigma1+mu1))(res1)
     
-    outputs = Conv2D(1, (1, 1), activation='linear')(conv7)
+    rec1 = layers.Lambda(ifft_layer)(res1_scaled)
+    final = layers.Lambda(lambda rec1 : (rec1-mu2)/sigma2)(rec1)
 
-    final = Model(inputs=inputs, outputs=outputs)
-    return final
+    model = Model(inputs=inputs, outputs=final)
+    return model
 
-# Displays messages to confirm initialization.
-print("\nInitializing...")
-print("Using Keras version", ks.__version__, "\n")
+stats, kspace_train, image_train, kspace_val, image_val, kspace_test, image_test = get_brains(num_train, num_val, num_test, addr)
 
-# Loads MRI data to the brains array.
-imshape = (256,256)
-norm = np.sqrt(imshape[0]*imshape[1])
-brains = []
-non_brains = []
-for brain in glob.glob('/Users/duncan.boyd/Documents/WorkCode/workvenv/MRIPractice/Train/*.npy') :
-    kspace = np.load(brain)/norm
-    rec = np.abs(np.fft.ifft2(kspace[:,:,:,0]+1j*kspace[:,:,:,1])).astype(np.float64)
-    if rec.shape == (170, 256, 256) :
-        brains.append(rec)
-brains = np.array(brains)
-print(brains.shape)
+# Declare, compile, fit the model.
+model = u_net(stats[0],stats[1],stats[2],stats[3])
+opt = tf.keras.optimizers.Adam(lr=1e-3,decay = 1e-7)
+model.compile(optimizer=opt, loss=nrmse)
 
-# Creates two normalized sets for training and testing.
-brains = brains / 35.46896
-brains = np.expand_dims(brains, axis=-1)
-train_set_rec = brains[0, 60:(60+TOTAL_PICS), :, :]
-train_set_dec = train_set_rec.copy()
-test_set_rec = brains[1, 80:(80+TEST_PICS), :, :]
-test_set_dec = test_set_rec.copy()
+# Some tools are skipped here (model loading, early stopping) as they aren't effective/necessary for small scale testing. 
 
-# Undersamples the deconstructed (initial) datasets.
-for i in range(TOTAL_PICS) :
-    for j in range(256) :
-        for k in range(256):
-            if j % SAMP_RATE == 0 or k % SAMP_RATE == 0 :
-                train_set_dec[i, j, k] = 0
-for i in range(TEST_PICS) :
-    for j in range(256) :
-        for k in range(256):
-            if j % SAMP_RATE == 0 or k % SAMP_RATE == 0 :
-                test_set_dec[i, j, k] = 0
+# Fits model using training data, validation data.
+model.fit(kspace_train, image_train, validation_data=(kspace_val, image_val), batch_size=BATCH_SIZE, epochs=EPOCHS)
+model.summary()
 
-# Prints shapes of
-print("Data in in range 0<x<1")
-print("Complete training: ", train_set_rec.shape)
-print("Incomplete training: ", train_set_dec.shape)
-print("Test set rec: ", test_set_rec.shape)
-print("Test set dec: ", test_set_dec.shape)
-
-# Calls U-Net model, compiles. 
-u_net =u_net(H=256,W=256,channels = 1,kshape = (3,3))
-u_net.compile(optimizer='adam',loss='mse',metrics=[ks.metrics.RootMeanSquaredError()])
-
-# Fits model using training data.
-u_net.fit(train_set_dec, train_set_rec, epochs=EPOCHS)
-
-# Predicts a set of reconstructed images.
-predictions = u_net.predict(test_set_dec)
+# Makes predictions
+predictions = model.predict(kspace_test)
 print(predictions.shape)
 
-# Plots an individual image from test and predictions.
-fig_num = 0
-while(fig_num != -1) :
-    plt.figure(figsize=(10,10))
-    plt.subplot(1,3,1)
-    plt.imshow(test_set_dec[fig_num], cmap='Greys')
-    plt.subplot(1,3,2)
-    plt.imshow(test_set_rec[fig_num], cmap='Greys')
-    plt.subplot(1,3,3)
-    plt.imshow(predictions[fig_num], cmap='Greys')
-    plt.show()
-    fig_num = int(input('Figure number:'))
+end_time = time.time()
 
-exit()
+# Displays predictions
+plt.figure(figsize=(15,15))
+plt.subplot(1,2,1)
+plt.imshow((255.0 - image_test[0]), cmap='Greys')
+plt.subplot(1,2,2)
+plt.imshow((255.0 - predictions[0]), cmap='Greys')
+plt.savefig("/Users/duncan.boyd/Documents/WorkCode/workvenv/UofC2022/SmallScaleTest/re_"+str(EPOCHS)+"_"+str(num_train)+"_"+str(MOD)+"_"+str(int(end_time-init_time))+".jpg")
+plt.show()
+
+
+
+
+
+
+
