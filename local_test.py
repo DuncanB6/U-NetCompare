Fixed normalization.

local_test.py
@@ -1,4 +1,4 @@
-# July 27, 2022
+# Aug 5, 2022
 
 # This program fits two unets and displays results. Used for local testing.
 
@@ -12,7 +12,6 @@ from unet_compare.real_unet import real_main
 from unet_compare.comp_unet import comp_main
 from unet_compare.functions import get_brains, mask_gen, normalize
 import logging
-import random
 
 # Import settings with hydra
 @hydra.main(
@@ -40,30 +39,6 @@ def main(cfg: DictConfig):
         kspace_test,
         image_test,
     ) = get_brains(cfg, ADDR)
-
-    # Following 3 blocks convert kspace to image domain.
-    # When implemented in a function, this code returned grey images.
-    aux = np.fft.ifft2(kspace_train[:, :, :, 0] + 1j * kspace_train[:, :, :, 1])
-    image = np.copy(kspace_train)
-    image[:, :, :, 0] = aux.real
-    image[:, :, :, 1] = aux.imag
-    kspace_train = image
-
-    aux = np.fft.ifft2(kspace_test[:, :, :, 0] + 1j * kspace_test[:, :, :, 1])
-    image = np.copy(kspace_test)
-    image[:, :, :, 0] = aux.real
-    image[:, :, :, 1] = aux.imag
-    kspace_test = image
-
-    aux = np.fft.ifft2(kspace_val[:, :, :, 0] + 1j * kspace_val[:, :, :, 1])
-    image = np.copy(kspace_val)
-    image[:, :, :, 0] = aux.real
-    image[:, :, :, 1] = aux.imag
-    kspace_val = image
-
-    plt.imshow(kspace_train[0, :, :, 0], cmap = 'gray')
-    plt.show()
-
 
     # Calls both models
     comp_model = comp_main(
@@ -96,12 +71,10 @@ def main(cfg: DictConfig):
     # Makes predictions
     logging.info("Evaluating UNet")
     comp_pred = comp_model.predict(kspace_test)
-    print(comp_pred.shape)
 
     # Makes predictions
     logging.info("Evaluating UNet")
     real_pred = real_model.predict(kspace_test)
-    print(real_pred.shape)
 
     # Displays predictions (Not necessary for ARC)
     plt.figure(figsize=(10, 10))

pred_test.py
@@ -1,6 +1,4 @@
-# This program is used to evaluate existing models. At the moment, it
-# only shows the models predictions, but in the future will be used
-# do do more detailed analysis.
+# This program is used to evaluate existing models.
 
 # Requires two existing unets, displays prediction images.
 
@@ -13,7 +11,7 @@ import hydra
 from omegaconf import DictConfig
 import numpy as np
 import matplotlib.pyplot as plt
-from unet_compare.functions import nrmse, CompConv2D, get_test, metrics, normalize
+from unet_compare.functions import nrmse, CompConv2D, get_test, metrics, normalize, mask_gen
 
 # Collects test data and uses it to evaluate existing models.
 @hydra.main(
@@ -25,19 +23,18 @@ def main(cfg: DictConfig):
 
     ADDR = Path.cwd()
 
+    # mask_gen(cfg, ADDR)
+
     # Loads data
     (
         kspace_test,
         image_test,
     ) = get_test(cfg, ADDR)
 
-    # Following 3 blocks convert kspace to image domain.
-    # When implemented in a function, this code returned grey images.
-    aux = np.fft.ifft2(kspace_test[:, :, :, 0] + 1j * kspace_test[:, :, :, 1])
-    image = np.copy(kspace_test)
-    image[:, :, :, 0] = aux.real
-    image[:, :, :, 1] = aux.imag
-    kspace_test = image
+    plt.imshow((255.0 - kspace_test[0, :, :, 0]), cmap='Greys')
+    plt.show()
+    plt.imshow((255.0 - image_test[0, :, :, 0]), cmap='Greys')
+    plt.show()
 
     comp_models = np.asarray(glob.glob(str(ADDR / cfg["addrs"]["COMP_ARC"])))
     real_models = np.asarray(glob.glob(str(ADDR / cfg["addrs"]["REAL_ARC"])))
@@ -62,27 +59,28 @@ def main(cfg: DictConfig):
 
         # Makes predictions
         comp_pred = comp_model.predict(kspace_test)
-        print(comp_pred.shape)
-
-        real_pred = np.zeros((cfg["params"]["NUM_TEST"], 256, 256, 2))
-        # Makes predictions
         real_pred = real_model.predict(kspace_test)
-        print(real_pred.shape)
 
         # Displays predictions (Not necessary for ARC)
         plt.figure(figsize=(10, 10))
         plt.subplot(1, 4, 1)
         plt.imshow((255.0 - image_test[0, :, :, 0]), cmap="Greys")
+        plt.axis("off")
         plt.subplot(1, 4, 2)
         plt.imshow((255.0 - comp_pred[0, :, :, 0]), cmap="Greys")
+        plt.axis("off")
         plt.subplot(1, 4, 3)
         plt.imshow((255.0 - real_pred[0, :, :, 0]), cmap="Greys")
+        plt.axis("off")
         plt.subplot(1, 4, 4)
         plt.imshow((255.0 - kspace_test[0, :, :, 0]), cmap="Greys")
+        plt.axis("off")
         plt.show()
 
+        comp_pred = comp_pred / np.max(np.abs(comp_pred[:, :, :, 0] + 1j * comp_pred[:, :, :, 1]))
+        real_pred = real_pred / np.max(np.abs(real_pred[:, :, :, 0] + 1j * real_pred[:, :, :, 1]))
         
-        comp_predc = np.sqrt(comp_pred[:, :, :, 0] * comp_pred[:, :, :, 0] + comp_pred[:, :, :, 1] * comp_pred[:, :, :, 1])
+        '''comp_predc = np.sqrt(comp_pred[:, :, :, 0] * comp_pred[:, :, :, 0] + comp_pred[:, :, :, 1] * comp_pred[:, :, :, 1])
         real_predc = np.sqrt(real_pred[:, :, :, 0] * real_pred[:, :, :, 0] + real_pred[:, :, :, 1] * real_pred[:, :, :, 1])
         image_testc = np.sqrt(image_test[:, :, :, 0] * image_test[:, :, :, 0] + image_test[:, :, :, 1] * image_test[:, :, :, 1]) 
 
@@ -93,13 +91,13 @@ def main(cfg: DictConfig):
         real_predc = real_predc.astype(np.float64)
 
         comp_predc = normalize(comp_predc, cd_bounds, a_bounds)
-        real_predc = normalize(real_predc, rd_bounds, a_bounds)
+        real_predc = normalize(real_predc, rd_bounds, a_bounds)'''
 
         # High score is better for SSIM, PSNR
         # Low is better for NRMSE
         # Should be ballpark (0.978 +/- 0.076, 1.827 +/- 1.112, 35.543 +/- 3.239)
         metrics_file = open(ADDR / cfg['addrs']['METRICS'], 'a')
-        metric = metrics(image_testc, comp_predc)
+        metric = metrics(image_test, comp_pred)
         metrics_file.write("\n\nComplex: ")
         metrics_file.write(str(comp_models[i]))
         metrics_file.write("\nSSIM: %.3f +/- %.3f" %(metric[:,0].mean(), metric[:,0].std()))
@@ -109,7 +107,7 @@ def main(cfg: DictConfig):
         comp_nrmse_sum += metric[:, 1].mean()
         comp_psnr_sum += metric[:, 2].mean()
 
-        metric = metrics(image_testc, real_predc)
+        metric = metrics(image_test, real_pred)
         metrics_file.write("\n\nReal: ")
         metrics_file.write(str(real_models[i]))
         metrics_file.write("\nSSIM: %.3f +/- %.3f" %(metric[:,0].mean(), metric[:,0].std()))

unet_compare/functions.py
@@ -13,7 +13,6 @@ import logging
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import random
 import sigpy.mri as sp # 1.22
-import matplotlib.pyplot as plt
 from skimage.metrics import structural_similarity as ssim
 from skimage.metrics import normalized_root_mse as norm_root_mse
 from skimage.metrics import peak_signal_noise_ratio as psnr
@@ -63,21 +62,23 @@ def get_test(cfg, ADDR):
     for ii in range(len(kspace_files_test)):
         ntest += np.load(kspace_files_test[ii]).shape[0]
 
-    # Load test data
+    # Load train data
     image_test = np.zeros((ntest, shape[0], shape[1], 2))
     kspace_test = np.zeros((ntest, shape[0], shape[1], 2))
     aux_counter = 0
     for ii in range(len(kspace_files_test)):
-        aux_kspace = np.load(kspace_files_test[ii]) / norm
-        aux = aux_kspace.shape[0]
-        aux2 = np.fft.ifft2(aux_kspace[:, :, :, 0] + 1j * aux_kspace[:, :, :, 1])
-        aux_kspace[:, mask[int(random.randint(0, cfg["params"]["NUM_MASKS"] - 1))], :] = 0
-        image_test[aux_counter : aux_counter + aux, :, :, 0] = aux2.real
-        image_test[aux_counter : aux_counter + aux, :, :, 1] = aux2.imag
-        kspace_test[aux_counter : aux_counter + aux, :, :, 0] = aux_kspace[:, :, :, 0]
-        kspace_test[aux_counter : aux_counter + aux, :, :, 1] = aux_kspace[:, :, :, 1]
+        dec_kspace = np.load(kspace_files_test[ii]) / norm
+        rec_kspace = np.copy(dec_kspace)
+        dec_kspace[:, mask[int(random.randint(0, cfg["params"]["NUM_MASKS"] - 1))], :] = 0
+        dec_image = np.fft.ifft2(dec_kspace[:, :, :, 0] + 1j * dec_kspace[:, :, :, 1])
+        rec_image = np.fft.ifft2(rec_kspace[:, :, :, 0] + 1j * rec_kspace[:, :, :, 1])
+        aux = rec_kspace.shape[0]
+        kspace_test[aux_counter : aux_counter + aux, :, :, 0] = dec_image.real
+        kspace_test[aux_counter : aux_counter + aux, :, :, 1] = dec_image.imag
+        image_test[aux_counter : aux_counter + aux, :, :, 0] = rec_image.real
+        image_test[aux_counter : aux_counter + aux, :, :, 1] = rec_image.imag
         aux_counter += aux
-
+    
     # Shuffle testing
     indexes = np.arange(image_test.shape[0], dtype=int)
     np.random.shuffle(indexes)
@@ -86,6 +87,9 @@ def get_test(cfg, ADDR):
 
     kspace_test = kspace_test[: cfg["params"]["NUM_TEST"], :, :, :]
     image_test = image_test[: cfg["params"]["NUM_TEST"], :, :, :]
+
+    kspace_test = kspace_test / np.max(np.abs(kspace_test[:, :, :, 0] + 1j * kspace_test[:, :, :, 1]))
+    image_test = image_test / np.max(np.abs(image_test[:, :, :, 0] + 1j * image_test[:, :, :, 1]))
 
     logging.info("kspace test: " + str(kspace_test.shape))
     logging.info("image test: " + str(image_test.shape))
@@ -255,13 +259,16 @@ def get_brains(cfg, ADDR):
     kspace_train = np.zeros((ntrain, shape[0], shape[1], 2))
     aux_counter = 0
     for ii in range(len(kspace_files_train)):
-        aux_kspace = np.load(kspace_files_train[ii]) / norm
-        aux = aux_kspace.shape[0]
-        aux2 = np.fft.ifft2(aux_kspace[:, :, :, 0] + 1j * aux_kspace[:, :, :, 1])
-        image_train[aux_counter : aux_counter + aux, :, :, 0] = aux2.real
-        image_train[aux_counter : aux_counter + aux, :, :, 1] = aux2.imag
-        kspace_train[aux_counter : aux_counter + aux, :, :, 0] = aux_kspace[:, :, :, 0]
-        kspace_train[aux_counter : aux_counter + aux, :, :, 1] = aux_kspace[:, :, :, 1]
+        dec_kspace = np.load(kspace_files_train[ii]) / norm
+        rec_kspace = np.copy(dec_kspace)
+        dec_kspace[:, mask[int(random.randint(0, cfg["params"]["NUM_MASKS"] - 1))], :] = 0
+        dec_image = np.fft.ifft2(dec_kspace[:, :, :, 0] + 1j * dec_kspace[:, :, :, 1])
+        rec_image = np.fft.ifft2(rec_kspace[:, :, :, 0] + 1j * rec_kspace[:, :, :, 1])
+        aux = rec_kspace.shape[0]
+        kspace_train[aux_counter : aux_counter + aux, :, :, 0] = dec_image.real
+        kspace_train[aux_counter : aux_counter + aux, :, :, 1] = dec_image.imag
+        image_train[aux_counter : aux_counter + aux, :, :, 0] = rec_image.real
+        image_train[aux_counter : aux_counter + aux, :, :, 1] = rec_image.imag
         aux_counter += aux
 
     # Shuffle training
@@ -273,7 +280,8 @@ def get_brains(cfg, ADDR):
     kspace_train = kspace_train[: cfg["params"]["NUM_TRAIN"], :, :, :]
     image_train = image_train[: cfg["params"]["NUM_TRAIN"], :, :, :]
 
-    kspace_train[:, mask[int(random.randint(0, cfg["params"]["NUM_MASKS"] - 1))], :] = 0
+    kspace_train = kspace_train / np.max(np.abs(kspace_train[:, :, :, 0] + 1j * kspace_train[:, :, :, 1]))
+    image_train = image_train / np.max(np.abs(image_train[:, :, :, 0] + 1j * image_train[:, :, :, 1]))
 
     logging.info("kspace train: " + str(kspace_train.shape))
     logging.info("image train: " + str(image_train.shape))
@@ -288,13 +296,16 @@ def get_brains(cfg, ADDR):
     kspace_val = np.zeros((nval, shape[0], shape[1], 2))
     aux_counter = 0
     for ii in range(len(kspace_files_val)):
-        aux_kspace = np.load(kspace_files_val[ii]) / norm
-        aux = aux_kspace.shape[0]
-        aux2 = np.fft.ifft2(aux_kspace[:, :, :, 0] + 1j * aux_kspace[:, :, :, 1])
-        image_val[aux_counter : aux_counter + aux, :, :, 0] = aux2.real
-        image_val[aux_counter : aux_counter + aux, :, :, 1] = aux2.imag
-        kspace_val[aux_counter : aux_counter + aux, :, :, 0] = aux_kspace[:, :, :, 0]
-        kspace_val[aux_counter : aux_counter + aux, :, :, 1] = aux_kspace[:, :, :, 1]
+        dec_kspace = np.load(kspace_files_val[ii]) / norm
+        rec_kspace = np.copy(dec_kspace)
+        dec_kspace[:, mask[int(random.randint(0, cfg["params"]["NUM_MASKS"] - 1))], :] = 0
+        dec_image = np.fft.ifft2(dec_kspace[:, :, :, 0] + 1j * dec_kspace[:, :, :, 1])
+        rec_image = np.fft.ifft2(rec_kspace[:, :, :, 0] + 1j * rec_kspace[:, :, :, 1])
+        aux = rec_kspace.shape[0]
+        kspace_val[aux_counter : aux_counter + aux, :, :, 0] = dec_image.real
+        kspace_val[aux_counter : aux_counter + aux, :, :, 1] = dec_image.imag
+        image_val[aux_counter : aux_counter + aux, :, :, 0] = rec_image.real
+        image_val[aux_counter : aux_counter + aux, :, :, 1] = rec_image.imag
         aux_counter += aux
 
     # Shuffle valing
@@ -307,7 +318,8 @@ def get_brains(cfg, ADDR):
     kspace_val = kspace_val[: cfg["params"]["NUM_VAL"], :, :, :]
     image_val = image_val[: cfg["params"]["NUM_VAL"], :, :, :]
 
-    kspace_val[:, mask[int(random.randint(0, cfg["params"]["NUM_MASKS"] - 1))], :] = 0
+    kspace_val = kspace_val / np.max(np.abs(kspace_val[:, :, :, 0] + 1j * kspace_val[:, :, :, 1]))
+    image_val = image_val / np.max(np.abs(image_val[:, :, :, 0] + 1j * image_val[:, :, :, 1]))
 
     logging.info("kspace val: " + str(kspace_val.shape))
     logging.info("image val: " + str(image_val.shape))
@@ -322,13 +334,16 @@ def get_brains(cfg, ADDR):
     kspace_test = np.zeros((ntest, shape[0], shape[1], 2))
     aux_counter = 0
     for ii in range(len(kspace_files_test)):
-        aux_kspace = np.load(kspace_files_test[ii]) / norm
-        aux = aux_kspace.shape[0]
-        aux2 = np.fft.ifft2(aux_kspace[:, :, :, 0] + 1j * aux_kspace[:, :, :, 1])
-        image_test[aux_counter : aux_counter + aux, :, :, 0] = aux2.real
-        image_test[aux_counter : aux_counter + aux, :, :, 1] = aux2.imag
-        kspace_test[aux_counter : aux_counter + aux, :, :, 0] = aux_kspace[:, :, :, 0]
-        kspace_test[aux_counter : aux_counter + aux, :, :, 1] = aux_kspace[:, :, :, 1]
+        dec_kspace = np.load(kspace_files_test[ii]) / norm
+        rec_kspace = np.copy(dec_kspace)
+        dec_kspace[:, mask[int(random.randint(0, cfg["params"]["NUM_MASKS"] - 1))], :] = 0
+        dec_image = np.fft.ifft2(dec_kspace[:, :, :, 0] + 1j * dec_kspace[:, :, :, 1])
+        rec_image = np.fft.ifft2(rec_kspace[:, :, :, 0] + 1j * rec_kspace[:, :, :, 1])
+        aux = rec_kspace.shape[0]
+        kspace_test[aux_counter : aux_counter + aux, :, :, 0] = dec_image.real
+        kspace_test[aux_counter : aux_counter + aux, :, :, 1] = dec_image.imag
+        image_test[aux_counter : aux_counter + aux, :, :, 0] = rec_image.real
+        image_test[aux_counter : aux_counter + aux, :, :, 1] = rec_image.imag
         aux_counter += aux
 
     # Shuffle testing
@@ -340,7 +355,8 @@ def get_brains(cfg, ADDR):
     kspace_test = kspace_test[: cfg["params"]["NUM_TEST"], :, :, :]
     image_test = image_test[: cfg["params"]["NUM_TEST"], :, :, :]
 
-    kspace_test[:, mask[int(random.randint(0, cfg["params"]["NUM_MASKS"] - 1))], :] = 0
+    kspace_test = kspace_test / np.max(np.abs(kspace_test[:, :, :, 0] + 1j * kspace_test[:, :, :, 1]))
+    image_test = image_test / np.max(np.abs(image_test[:, :, :, 0] + 1j * image_test[:, :, :, 1]))
 
     logging.info("kspace test: " + str(kspace_test.shape))
     logging.info("image test: " + str(image_test.shape))

unet_compare/real_unet.py
@@ -41,7 +41,7 @@ def real_main(
         monitor="val_loss",
         save_best_only=True,
     )
-    es = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=20, mode="min")
+    es = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=5, mode="min")
     csvl = tf.keras.callbacks.CSVLogger(
         str(ADDR / cfg["addrs"]["REAL_CSV"]), append=False, separator="|"
     )