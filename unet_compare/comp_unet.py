8001c04416f5d4802debb9b00d214ab284af79d7
Duncan Boyd <duncanboyd@Duncans-MacBook-Pro.local>
2022-08-13 09:33:40 -0600

    Masking changes.

--- unet_compare/comp_unet.py
+++ unet_compare/comp_unet.py
@@ -42,7 +42,7 @@ def comp_main(
         monitor="val_loss",
         save_best_only=True,
     )
-    es = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=5, mode="min")
+    es = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=20, mode="min")
     csvl = tf.keras.callbacks.CSVLogger(
         str(ADDR / cfg["addrs"]["COMP_CSV"]), append=False, separator="|"
     )
